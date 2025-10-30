78fe1a7ac3580370ab92c052e4cf5abdebcc0f86
Kate Haravaya <kateazhevskaya@gmail.com>
2025-10-30 16:02:25 +0100

    did Physics Control

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Lander : MonoBehaviour {
+
+
+    private Rigidbody2D landerRigidbody2D;
+    
+    private void Awake() {
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    private void FixedUpdate()
+    {
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            float force = 700f;
+            landerRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
+        }
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            float turnSpeed = +100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+        if (Keyboard.current.rightArrowKey.isPressed) {
+            float turnSpeed = -100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+    }
+}
